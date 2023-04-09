47d93c6f91e1def3a9c514616409a5854d71643d
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-04-09 17:51:03 +0200

    updated some dependcies and solved some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+    Home,
+    Products,
+    SingleProduct,
+    About,
+    Cart,
+    Error,
+    Checkout,
+    PrivateRoute,
+} from "./pages";
+
 const App = () => {
-    return <div>Comfy sloth store</div>;
+    return (
+        <Router>
+            <Navbar />
+            <Sidebar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/products/:id" element={<SingleProduct />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 };
 export default App;
