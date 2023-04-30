f368c532fd862e62334a7ee18a9bdcb45b67754b
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-04-30 18:54:11 +0300

    feat: filters products completed

--- src/reducers/filter_reducer.jsx
+++ src/reducers/filter_reducer.jsx
@@ -11,10 +11,13 @@ import {
 
 const filter_reducer = (state, action) => {
     if (action.type === LOAD_PRODUCTS) {
+        let maxPrice = action.payload.map((a) => a.price);
+        maxPrice = Math.max(...maxPrice);
         return {
             ...state,
             all_products: [...action.payload],
             filtered_products: [...action.payload],
+            filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
         };
     }
 
@@ -63,6 +66,74 @@ const filter_reducer = (state, action) => {
         }
 
         return { ...state, filtered_products: tempProducts };
+    }
+
+    if (action.type === UPDATE_FILTERS) {
+        const { name, value } = action.payload;
+        return { ...state, filters: { ...state.filters, [name]: value } };
+    }
+
+    if (action.type === FILTER_PRODUCTS) {
+        const { all_products } = state;
+        const { text, category, company, color, price, shipping } =
+            state.filters;
+
+        let tempProducts = [...all_products];
+
+        // Filters
+        // Search
+        if (text) {
+            tempProducts = tempProducts.filter((product) => {
+                return product.name.toLowerCase().startsWith(text);
+            });
+        }
+
+        // Category
+        if (category !== "all") {
+            tempProducts = tempProducts.filter(
+                (product) => product.category === category
+            );
+        }
+
+        // Company
+        if (company !== "all") {
+            tempProducts = tempProducts.filter(
+                (product) => product.company === company
+            );
+        }
+
+        // Colors
+        if (color !== "all") {
+            tempProducts = tempProducts.filter((product) => {
+                return product.colors.find((c) => color === c);
+            });
+        }
+
+        // Price
+        tempProducts = tempProducts.filter((product) => product.price <= price);
+        // Shipping
+        if (shipping) {
+            tempProducts = tempProducts.filter(
+                (product) => product.shipping === true
+            );
+        }
+
+        return { ...state, filtered_products: tempProducts };
+    }
+
+    if (action.type === CLEAR_FILTERS) {
+        return {
+            ...state,
+            filters: {
+                ...state.filters,
+                text: "",
+                company: "all",
+                category: "all",
+                color: "all",
+                price: state.filters.max_price,
+                shipping: false,
+            },
+        };
     }
 
     throw new Error(`No Matching "${action.type}" - action type`);
