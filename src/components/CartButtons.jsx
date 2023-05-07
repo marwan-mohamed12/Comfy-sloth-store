05e4a1c5033f1526eeacc23cdad10aeee7256a68
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-05-07 19:15:25 +0300

    feat: completed the authentication part

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
+import {
+    FaMinus,
+    FaShoppingCart,
+    FaUserMinus,
+    FaUserPlus,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useProductsContext } from "../context/products_context";
@@ -9,6 +14,7 @@ import { useUserContext } from "../context/user_context";
 const CartButtons = () => {
     const { closeSidebar } = useProductsContext();
     const { total_items } = useCartContext();
+    const { loginWithRedirect, logout, myUser } = useUserContext();
     return (
         <Wrapper className="cart-btn-wrapper" onClick={closeSidebar}>
             <Link to={"/cart"} className="cart-btn">
@@ -18,9 +24,23 @@ const CartButtons = () => {
                     <span className="cart-value">{total_items}</span>
                 </span>
             </Link>
-            <button type="button" className="auth-btn">
-                Login <FaUserPlus />
-            </button>
+            {myUser ? (
+                <button
+                    type="button"
+                    className="auth-btn"
+                    onClick={() => logout({ returnTo: window.location.origin })}
+                >
+                    Logout <FaUserMinus />
+                </button>
+            ) : (
+                <button
+                    type="button"
+                    className="auth-btn"
+                    onClick={loginWithRedirect}
+                >
+                    Login <FaUserPlus />
+                </button>
+            )}
         </Wrapper>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,30 @@ import "./index.css";
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { UserProvider } from "./context/user_context";
+
+// dev-n1p4fut4cfg013w1.us.auth0.com
+//219DSAqJXapkK1NHnrzXsZl7LGiteEdR
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <ProductsProvider>
-            <FilterProvider>
-                <CartProvider>
-                    <App />
-                </CartProvider>
-            </FilterProvider>
-        </ProductsProvider>
+        <Auth0Provider
+            domain="dev-n1p4fut4cfg013w1.us.auth0.com"
+            clientId="219DSAqJXapkK1NHnrzXsZl7LGiteEdR"
+            authorizationParams={{
+                redirect_uri: window.location.origin,
+            }}
+        >
+            <UserProvider>
+                <ProductsProvider>
+                    <FilterProvider>
+                        <CartProvider>
+                            <App />
+                        </CartProvider>
+                    </FilterProvider>
+                </ProductsProvider>
+            </UserProvider>
+        </Auth0Provider>
     </React.StrictMode>
 );
